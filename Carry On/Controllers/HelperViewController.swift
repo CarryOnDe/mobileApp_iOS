47a36e00075dc8366ddf8c571b245b041adb6675
Jonas <jonaslutsch@live.de>
2020-03-21 12:21:08 +0100

    add missing files

--- Carry On/Controllers/HelperViewController.swift
+++ Carry On/Controllers/HelperViewController.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 Jonas. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HelperViewController: UIViewController {
+
+    let entryArray = ["Erntehelfer", "Einkaufshilfe"]
+    
+    @IBOutlet var helpRequestTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        helpRequestTable.dataSource = self
+        helpRequestTable.delegate = self
+        
+    }
+
+}
+
+extension HelperViewController:
+    UITableViewDataSource,
+    UITableViewDelegate
+{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entryArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! HelpTableViewCell
+        cell.helpRequestTitle.text = entryArray[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+    
+}
+
+
+class HelpTableViewCell: UITableViewCell {
+    
+    @IBOutlet var helpRequestTitle: UILabel!
+    
+    
+}
